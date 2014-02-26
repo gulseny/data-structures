3046e8b0dbc788266ba5a46df8517315f1ef06c8
Hack Reactor Students <gsenol@gmail.com>
2014-02-26 15:47:41 -0800

    completed functional-shared QUEUE

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,5 +1,29 @@
+
 var makeQueue = function(){
-  // Hey! Copy your code from src/functional/queue.js and paste it here
+  var instance = {};
+
+  // Use an object with numeric keys to store values
+  instance.storage = {};
+  instance.rank = 0;
+
+  _.extend(instance, queueMethods);
+
+  return instance;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value){
+    this.storage[this.rank++] = value;
+  };
+
+queueMethods.dequeue = function(){
+    var key = Object.keys(this.storage)[0];
+    var value = this.storage[key];
+    delete this.storage[key];
+    return value;
+  };
+
+queueMethods.size = function(){
+    return Object.keys(this.storage).length;
+  };
