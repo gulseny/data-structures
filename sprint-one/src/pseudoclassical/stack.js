5095629595a0347899d3c58bd621ba0a3104f96f
Hack Reactor Students <gsenol@gmail.com>
2014-02-26 16:22:56 -0800

    completed pseudoclassical pattern of Stack

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,3 +1,21 @@
 var Stack = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+  this.storage = {};
+  this.stackSize = 0;
+};
+
+Stack.prototype.push = function(value){
+  this.storage[this.stackSize++] = value;
+};
+
+Stack.prototype.pop = function(){
+  var value = this.storage[this.stackSize - 1];
+  if(this.stackSize > 0){
+    delete this.storage[this.stackSize - 1];
+    this.stackSize--;
+  }
+  return value;
+};
+
+Stack.prototype.size = function(){
+  return this.stackSize;
 };
