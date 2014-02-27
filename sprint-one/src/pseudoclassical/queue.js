3fa6d10e140ecc8975e700846da8646e459ff78a
Hack Reactor Students <gsenol@gmail.com>
2014-02-26 16:20:01 -0800

    completed pseudoclassical pattern of Queue

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,3 +1,22 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.storage = {};
+  this.rank = 0;
 };
+
+Queue.prototype.enqueue = function(value){
+    this.storage[this.rank++] = value;
+  };
+
+Queue.prototype.dequeue = function(){
+    var key = Object.keys(this.storage)[0];
+    var value = this.storage[key];
+    delete this.storage[key];
+    return value;
+  };
+
+Queue.prototype.size = function(){
+    return Object.keys(this.storage).length;
+  };
+
+
+
