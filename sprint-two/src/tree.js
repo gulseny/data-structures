ea47a31c87e28fb65ef20ae9f48743a39513f37e
Hack Reactor Students <gsenol@gmail.com>
2014-02-27 20:23:38 -0800

    completed solution for Tree with functional instantiation

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,16 +1,24 @@
 var makeTree = function(value){
-  var newTree = {};
-  newTree.value = value;
-  newTree.children = undefined;
-  return newTree;
+  return {
+    value: value,
+    children: undefined,
+
+    addChild: function(value){
+      var aSubTree = makeTree(value);
+      if(this.children){
+        this.children.push(aSubTree);
+      } else {
+        this.children = [aSubTree];
+      }
+    },
+
+    contains: function(target){
+      var isFound = this.value === target;
+      for (var i = 0; !isFound && this.children && i < this.children.length; i++) {
+        isFound = this.children[i].contains(target);
+      }
+      return isFound;
+    }
+  };
+
 };
-
-
-var treeMethods = {};
-
-treeMethods.addChild = function(value){
-};
-
-treeMethods.contains = function(target){
-};
-
