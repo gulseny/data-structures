68f0022a96832b39af5a84651c5067bd514440bc
Hack Reactor Students <gsenol@gmail.com>
2014-02-27 20:41:07 -0800

    complete tree in functional-sharing

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,24 +1,35 @@
 var makeTree = function(value){
-  return {
+  var tree = {
     value: value,
-    children: undefined,
-
-    addChild: function(value){
-      var aSubTree = makeTree(value);
-      if(this.children){
-        this.children.push(aSubTree);
-      } else {
-        this.children = [aSubTree];
-      }
-    },
-
-    contains: function(target){
-      var isFound = this.value === target;
-      for (var i = 0; !isFound && this.children && i < this.children.length; i++) {
-        isFound = this.children[i].contains(target);
-      }
-      return isFound;
-    }
+    children: undefined
   };
 
+  extend(tree, treeMethods);
+  return tree;
+};
+
+var extend = function(aTree, treeMethods){
+  for(var method in treeMethods){
+    aTree[method] = treeMethods[method];
+  }
+  return aTree;
+};
+
+var treeMethods = {};
+
+treeMethods.addChild = function(value){
+  var aSubTree = makeTree(value);
+  if(this.children){
+    this.children.push(aSubTree);
+  } else {
+    this.children = [aSubTree];
+  }
+};
+
+treeMethods.contains = function(target){
+  var isFound = this.value === target;
+  for (var i = 0; !isFound && this.children && i < this.children.length; i++) {
+    isFound = this.children[i].contains(target);
+  }
+  return isFound;
 };
