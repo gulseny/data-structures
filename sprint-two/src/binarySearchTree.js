918855ed85b82a93a191e0f51f35220e8f0a5949
Hack Reactor Students <gsenol@gmail.com>
2014-02-28 18:39:50 -0800

    extra-credit: Binary Search Tree first shot passing specs

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,2 +1,31 @@
 var makeBinarySearchTree = function(value){
+  var newBST = {};
+  newBST.value = value;
+  newBST.left = undefined;
+  newBST.right = undefined;
+
+  extend(newBST, makeBinarySearchTree.prototype);
+
+  return newBST;
 };
+
+makeBinarySearchTree.prototype.insert = function(value){
+  if(value && value !== this.value){
+    var side = (value < this.value) ? 'left' : 'right';
+    if(!this[side]){
+      this[side] = makeBinarySearchTree(value);
+    } else {
+      this[side].insert(value);
+    }  
+  }
+};
+
+makeBinarySearchTree.prototype.contains = function(value){
+  if(value === this.value){
+    return true;
+  }
+  var side = (value < this.value) ? 'left' : 'right';
+  return !!this[side] && this[side].contains(value);
+};
+
+makeBinarySearchTree.prototype.depthFirstLog = function(){};
