2a418b8bc01b1c25578a7dfc1dcfd67fa0091919
Hack Reactor Students <gsenol@gmail.com>
2014-02-28 20:34:11 -0800

    extra-credit: transform hash table for dynamic sizing

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,21 +1,38 @@
 var HashTable = function(){
   this._limit = 8;
+  this._count = 0;
   this._storage = makeLimitedArray(this._limit);
 };
 
 HashTable.prototype.insert = function(k, v){
-  var i = getIndexBelowMaxForKey(k, this._limit);
-  this._storage.set(i, v);
+  var moveStorage = function(keyValue){
+    if(keyValue){
+      var newIndex = getIndexBelowMaxForKey(keyValue[0], newLimit);
+      newStorage.set(newIndex, [keyValue[0],keyValue[1]]);
+    }
+  };
+  if(this._count/(this._limit-1) > 0.75){
+    this._limit *= 2; 
+    var newLimit = this._limit ;
+    var newStorage = makeLimitedArray(newLimit);
+    this._storage.each(moveStorage);
+    this._storage = newStorage;
+  } else {
+    var i = getIndexBelowMaxForKey(k, this._limit);
+    this._storage.set(i, [k,v]);
+    this._count++;
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
-  return this._storage.get(i);
+  return (this._storage.get(i))?this._storage.get(i)[1]:null;
 };
 
 HashTable.prototype.remove = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
   var removedValue = this._storage.get(i);
   this._storage.set(i, null);
+  this._count--;
   return removedValue;
 };
