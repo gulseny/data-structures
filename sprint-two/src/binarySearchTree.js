c458b3fdaa096eff30910b1293cd32244a8b447d
Hack Reactor Students <gsenol@gmail.com>
2014-02-28 19:09:29 -0800

    extra-credit: binary search tree, depthFirstLog()

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -28,4 +28,17 @@ makeBinarySearchTree.prototype.contains = function(value){
   return !!this[side] && this[side].contains(value);
 };
 
-makeBinarySearchTree.prototype.depthFirstLog = function(){};
+// depthFirstLog(): accepts a callback and executes it on every value contained in the tree
+makeBinarySearchTree.prototype.depthFirstLog = function(callback){
+  callback(this.value);
+
+  // we aim to call the callback function on the current subtree
+
+
+  if(this.left){
+    this.left.depthFirstLog(callback);
+  }
+  if(this.right){
+    this.right.depthFirstLog(callback);
+  }
+};
