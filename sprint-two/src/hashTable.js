63da999a6d0392f252055810c93640de54c9bb0b
Hack Reactor Students <gsenol@gmail.com>
2014-03-01 11:00:15 -0800

    extra credit: factorize dynamic size

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -4,22 +4,22 @@ var HashTable = function(){
   this._storage = makeLimitedArray(this._limit);
 };
 
-var moveStorage = function(storage){
-  if(storage.keyValue){
-    var newIndex = getIndexBelowMaxForKey(storage.keyValue[0], storage.limit);
-    storage.storage.set(newIndex, [storage.keyValue[0],storage.keyValue[1]]);
-  }
+HashTable.prototype.updateSize = function(){
+  var newLimit = this._limit * ((this._count/this._limit > 0.5) ? 2 : 0.5);
+  this._limit = newLimit;
+  var newStorage = makeLimitedArray(newLimit);
+  this._storage.each(function(keyValue){
+    if(keyValue){
+      var newIndex = getIndexBelowMaxForKey(keyValue[0], newLimit);
+      newStorage.set(newIndex, [keyValue[0],keyValue[1]]);
+    }
+  });
+  this._storage = newStorage;
 };
 
 HashTable.prototype.insert = function(k, v){
   if(this._count/this._limit >= 0.75){
-    this._limit *= 2;
-    var newLimit = this._limit ;
-    var newStorage = makeLimitedArray(newLimit);
-    this._storage.each(function(keyValue){
-     moveStorage({keyValue: keyValue, limit: newLimit, storage: newStorage});
-    });
-    this._storage = newStorage;
+    this.updateSize();
   } else {
     var i = getIndexBelowMaxForKey(k, this._limit);
     this._storage.set(i, [k,v]);
@@ -38,13 +38,7 @@ HashTable.prototype.remove = function(k){
   this._storage.set(i, null);
   this._count--;
   if(this._count/this._limit < 0.25 && (2*this._count/this._limit < 0.25)){
-    this._limit /= 2;
-    var newLimit = this._limit ;
-    var newStorage = makeLimitedArray(newLimit);
-    this._storage.each(function(keyValue){
-     moveStorage({keyValue: keyValue, limit: newLimit, storage: newStorage});
-    });
-    this._storage = newStorage;
+    this.updateSize();
   }
   return removedValue;
 };
